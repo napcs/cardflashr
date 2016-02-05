856043e20feaeea5c0bea02be03c92e54f89bee8
Brian Hogan <brianhogan@napcs.com>
2016-02-05 09:19:59 -0600

    alter tests to support logins

--- test/controllers/decks_controller_test.rb
+++ test/controllers/decks_controller_test.rb
@@ -3,20 +3,24 @@ require 'test_helper'
 class DecksControllerTest < ActionController::TestCase
   setup do
     @deck = decks(:private)
+    @user = @deck.user
   end
 
   test "should get index" do
+    sign_in @user
     get :index
     assert_response :success
     assert_not_nil assigns(:decks)
   end
 
   test "should get new" do
+    sign_in @user
     get :new
     assert_response :success
   end
 
   test "should create deck" do
+    sign_in @user
     assert_difference('Deck.count') do
       post :create, deck: { description: @deck.description, name: @deck.name, shared: @deck.shared, user_id: @deck.user_id }
     end
@@ -25,21 +29,25 @@ class DecksControllerTest < ActionController::TestCase
   end
 
   test "should show deck" do
+    sign_in @user
     get :show, id: @deck
     assert_response :success
   end
 
   test "should get edit" do
+    sign_in @user
     get :edit, id: @deck
     assert_response :success
   end
 
   test "should update deck" do
+    sign_in @user
     patch :update, id: @deck, deck: { description: @deck.description, name: @deck.name, shared: @deck.shared, user_id: @deck.user_id }
     assert_redirected_to deck_path(assigns(:deck))
   end
 
   test "should destroy deck" do
+    sign_in @user
     assert_difference('Deck.count', -1) do
       delete :destroy, id: @deck
     end
--- test/controllers/player_controller_test.rb
+++ test/controllers/player_controller_test.rb
@@ -1,8 +1,14 @@
 require 'test_helper'
 
 class PlayerControllerTest < ActionController::TestCase
+  setup do
+    @deck = decks(:private)
+    @user = @deck.user
+  end
+
   test "should get show" do
-    get :show
+    sign_in @user
+    get :show, deck_id: @deck
     assert_response :success
   end
 
