43045ca65bafd81a751919bc93c394a708ad4237
Brian Hogan <brianhogan@napcs.com>
2016-02-22 09:50:58 -0600

    fix broken tests

--- test/controllers/categories_controller_test.rb
+++ test/controllers/categories_controller_test.rb
@@ -2,21 +2,25 @@ require 'test_helper'
 
 class CategoriesControllerTest < ActionController::TestCase
   setup do
-    @category = categories(:one)
+    @category = categories(:math)
+    @user = User.first
   end
 
   test "should get index" do
+    sign_in @user
     get :index
     assert_response :success
     assert_not_nil assigns(:categories)
   end
 
   test "should get new" do
+    sign_in @user
     get :new
     assert_response :success
   end
 
   test "should create category" do
+    sign_in @user
     assert_difference('Category.count') do
       post :create, category: { name: @category.name }
     end
@@ -25,21 +29,25 @@ class CategoriesControllerTest < ActionController::TestCase
   end
 
   test "should show category" do
+    sign_in @user
     get :show, id: @category
     assert_response :success
   end
 
   test "should get edit" do
+    sign_in @user
     get :edit, id: @category
     assert_response :success
   end
 
   test "should update category" do
+    sign_in @user
     patch :update, id: @category, category: { name: @category.name }
     assert_redirected_to category_path(assigns(:category))
   end
 
   test "should destroy category" do
+    sign_in @user
     assert_difference('Category.count', -1) do
       delete :destroy, id: @category
     end
